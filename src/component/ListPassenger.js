2b2e7bf0aa27e123a0b9b007cf25f25d8253a91a
JeremyManurung <87016966+JeremyManurung@users.noreply.github.com>
2021-09-22 09:19:19 +0700

    Update ListPassenger.js

--- src/component/ListPassenger.js
+++ src/component/ListPassenger.js
@@ -1,63 +1,15 @@
 import ListItem from './ListItem';
 import {gql, useQuery, useLazyQuery} from '@apollo/client'
 import LoadingSvg from "./LoadingSvg"
-import { useState } from "react"
+import PassengerInput from './PassengerInput';
+import { useState } from 'react';
 
-
-const GetData = gql`
-    query MyQuery {
-    anggota {
-    id
-    jenis_kelamin
-    nama
-    umur
-    }
-}
-`
-const GetDataByUserId = gql `
-    query MyQuery($id: Int!) {
-    anggota(where: {id: {_eq: $id}}) {
-        nama
-        umur
-        jenis_kelamin
-        id
-    }
-    }
-`;
 
 const ListPassenger = props => {
-        const [getData,{data, loading, error}] = useLazyQuery(GetDataByUserId)
-        const [dataId,setDataId] = useState(0);
-
-        if(loading){
-            return <LoadingSvg/>
-        }
-
-        if (error){
-            console.log(error)
-            return null
-        }
-
-        const onGetData = () => {
-            console.log("masuk = ", dataId);
-            getData({variables : {
-                id : dataId
-            }})
-            // getDatanya belum di masukkan query dan vairabel
-            // this.setState(data?.anggota);
-        }
-
-        const onChangeDataId = (e) =>{
-            if(e.target){
-                setDataId(e.target.value)
-            }
-        }
-    
-    
+    console.log(props.data)
+        
     return (
         <div>
-            <input value={dataId} onChange={onChangeDataId}/>
-            <button onClick={onGetData}>Get Data</button>
             <table cellPadding="5px" cellSpacing="0" style={{margin: "auto"}}>
                 <thead bgcolor="red">
                     <td>Nama</td>
@@ -65,7 +17,7 @@ const ListPassenger = props => {
                     <td>Jenis Kelamin</td>
                     <td bgcolor="white" className="removeBorder"></td>
                 </thead>
-                {data?.anggota.map(item => (
+                {props.data?.map(item => (
                     <ListItem
                         key={item.id}
                         data={item}
