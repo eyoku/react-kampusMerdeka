7d033eabf134cf317a3ea4e52bcd52705a2dfe15
JeremyManurung <87016966+JeremyManurung@users.noreply.github.com>
2021-09-22 09:20:59 +0700

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,81 +3,123 @@ import { v4 as uuidv4 } from "uuid";
 import PassengerInput from './PassengerInput';
 import ListPassenger from './ListPassenger';
 import Header from './Header';
+import {gql, useQuery, useLazyQuery, useMutation} from '@apollo/client';
+import { useState } from 'react';
+import LoadingSvg from "./LoadingSvg"
 
+const initialValue = {
+    nama: "",
+    umur: "",
+    jeniskelamin: ""
+}
 
-class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data : [
-                {
-                    id: uuidv4(),
-                    nama: 'Yoga',
-                    umur: 22,
-                    jenisKelamin: 'Pria'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Ria',
-                    umur: 19,
-                    jenisKelamin: 'Wanita'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Fahmi',
-                    umur: 25,
-                    jenisKelamin: 'Pria'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Lala',
-                    umur: 21,
-                    jenisKelamin: 'Wanita'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Ivan',
-                    umur: 25,
-                    jenisKelamin: 'Pria'
-                }
-            ]
+const GetData = gql`
+    query MyQuery {
+        anggota {
+        id
+        jenis_kelamin
+        nama
+        umur
         }
+    }      
+    `
+
+function Home () {
+
+    const deleteData = gql`
+    mutation MyMutation($id: Int!) {
+    delete_anggota_by_pk(id: $id) {
+        id
+    }
+    }
+`;
+
+const insertData = gql`
+mutation MyMutation($jenis_kelamin: String!, $nama: String! $umur: Int!, $id: Int!) {
+    insert_anggota(objects: {jenis_kelamin: $jenis_kelamin, nama: $nama, umur: $umur, id: $id}) {
+      returning {
+        jenis_kelamin
+        nama
+        umur
+        id
+      }
+    }
+  }
+`
+    
+    const GetDataByUserId = gql `
+        query MyQuery($id: Int!) {
+        anggota(where: {id: {_eq: $id}}) {
+            nama
+            umur
+            jenis_kelamin
+            id
+        }
+        }
+    `;
+    
+    const [list, setList] = useState([])
+    const [deleteDataN, {loading : loadingDelete}] = useMutation(deleteData,{
+        refetchQueries: [GetData]
+    });
+    const [insertDataN, {loading:loadingInsert}] = useMutation(insertData, {
+    refetchQueries: [GetData]
+  })
+    const {data, loading, error} = useQuery(GetData)
+    const [input, setInput] = useState("")
+
+
+    if(loading || loadingDelete || loadingInsert){
+        return <LoadingSvg/>
     }
 
-    hapusPengunjung = id => {
-        this.setState({    
-            data: [      
-                ...this.state.data.filter(item => {        
-                    return item.id !== id;      
-                })    
-            ]  
-        });
-    };
-    
-    tambahPengunjung = newUser => {
-        const newData = {
-            id: uuidv4(),
-            ...newUser
-        }; 
-        this.setState({    
-            data: [...this.state.data, newData]  
-        });
-    };
-    
-    render() {
-        return (
-            <div>
-                <Header/>
-                <ListPassenger 
-                    data={this.state.data}
-                    hapusPengunjung={this.hapusPengunjung}
-                />
-                <PassengerInput
-                    tambahPengunjung={this.tambahPengunjung}
-                />
-            </div>
-        )
+    if (error){
+        console.log("error ",error)
+        return null
     }
+    
+
+    const hapusPengunjung = id => {
+    deleteDataN({variables :{
+      id:id
+    }})
+  };
+
+      const tambahPengunjung = newUser => {
+        console.log(newUser.nama)
+        const newData = {
+            ...newUser
+        }
+        // console.log(newData)
+        insertDataN({variables :{
+              id: newData.id,
+              nama: newData.nama,
+              umur: newData.umur,
+              jenis_kelamin: newData.jenisKelamin
+        }})
+    }
+    // const onGetData = () =>{
+    //     getData_qry({variables : {
+    //         id : input
+    //     }})
+
+    //     console.log(data?.anggota)
+    //     // setList(data?.anggota)
+    //   }
+    
+    // const onchangeInput = (e) => {
+    //     setInput(e.target.value)
+    // }
+    return(
+        <div>
+            <Header />
+            {/* <input type="text" onChange={onchangeInput} />
+            <button onClick={onGetData}>Get Data</button> */}
+            <ListPassenger data={data?.anggota} hapusPengunjung={hapusPengunjung} />
+            <PassengerInput tambahPengunjung={tambahPengunjung}/>
+        </div>
+    )
+    
 }
 
 export default Home;
